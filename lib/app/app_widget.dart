3e68281c082a74c60b60e7e2ae06a000b6b90980
Carlos Doki <carlosdoki@gmail.com>
2024-02-15 14:46:46 -0300

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'modules/splash/splash_page.dart';
+
+class AppWidget extends StatefulWidget {
+  const AppWidget({super.key});
+
+  @override
+  State<AppWidget> createState() => _AppWidgetState();
+}
+
+class _AppWidgetState extends State<AppWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Todo List Provider',
+      home: SplashPage(),
+    );
+  }
+}
